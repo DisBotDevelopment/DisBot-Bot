cb4a6eac6b8e5e5a54b64d46adfa4d006714cf35
xyzjesper <dm@xyzjesper>
2025-08-15 21:31:21 +0200

    Add DisBot entrypoint.sh ready docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,13 +9,8 @@ export TZ=UTC
 # Get the internal IP address
 export INTERNAL_IP=$(ip route get 1 | awk '{print $NF; exit}')
 
-# Display NodeJS version
-echo "NodeJS Version:"
-node -v
-
 # Replace startup variables
 PARSED=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-
 
 # Run the main process
 eval ${PARSED}
