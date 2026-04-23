82cb0f7cfc52bef808d0f42a97e6bce76cd8169e
jesperrichert <jesper.l.richert@gmail.com>
2026-04-23 12:18:53 +0200

    changed using for migrations.

--- DisBot.API/Startup/Startup.Database.cs
+++ DisBot.API/Startup/Startup.Database.cs
@@ -13,17 +13,20 @@ public static partial class Startup
     private static async Task LoadDatabase(this WebApplication application)
     {
         application.Logger.Log(LogLevel.Information, "Loading database...");
-        
-        await using var scope = application.Services.CreateAsyncScope();
-        var dataContext = scope.ServiceProvider.GetService<DataContext>();
-        var migrations = await dataContext.Database.GetPendingMigrationsAsync();
-        if (dataContext == null) throw new Exception("No Database Context found...");
-        application.Logger.Log(LogLevel.Information, "Database initialized.");
-        if (migrations.ToArray().Length > 0)
+
+        await using (var scope = application.Services.CreateAsyncScope())
         {
-            await dataContext.Database.MigrateAsync();
-            application.Logger.Log(LogLevel.Information, "Database migrated.");
+            var dataContext = scope.ServiceProvider.GetService<DataContext>();
+            var migrations = await dataContext.Database.GetPendingMigrationsAsync();
+            if (dataContext == null) throw new Exception("No Database Context found...");
+            application.Logger.Log(LogLevel.Information, "Database initialized.");
+            if (migrations.ToArray().Length > 0)
+            {
+                await dataContext.Database.MigrateAsync();
+                application.Logger.Log(LogLevel.Information, "Database migrated.");
+            }
+
+            application.Logger.Log(LogLevel.Information, "Database ready to accept connections.");
         }
-        application.Logger.Log(LogLevel.Information, "Database ready to accept connections.");
     }
 }
