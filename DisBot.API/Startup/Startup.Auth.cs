9ac1b8e93cbf1925d64e4919ae054c7eb3e93b66
jesperrichert <jesper.l.richert@gmail.com>
2026-04-23 13:08:33 +0200

    Added controllers and auth loading.

--- DisBot.API/Startup/Startup.Auth.cs
+++ DisBot.API/Startup/Startup.Auth.cs
@@ -5,9 +5,12 @@ public static partial class Startup
     private static async Task InitialiseAuth(this WebApplicationBuilder builder)
     {
         builder.Services.AddAuthentication();
+        builder.Services.AddAuthorization();
     }
 
-    private static async Task LoadAuth(this WebApplication host)
+    private static async Task LoadAuth(this WebApplication application)
     {
+        application.UseAuthentication();
+        application.UseAuthorization();
     }
 }
--- DisBot.API/Startup/Startup.Base.cs
+++ DisBot.API/Startup/Startup.Base.cs
@@ -8,7 +8,6 @@ public partial class Startup
     private static async Task InitialiseBase(this WebApplicationBuilder builder)
     {
         builder.Services.AddControllers();
-        
         builder.Services.AddOptions<DatabaseOptions>().BindConfiguration("Database");
         builder.Services.AddOptions<OAuth2Options>().BindConfiguration("OAuth2");
         builder.Services.AddOptions<SessionOptions>().BindConfiguration("Session");
@@ -18,5 +17,7 @@ public partial class Startup
     private static async Task LoadBase(this WebApplication application)
     {
         application.MapOpenApi();
+        application.MapControllers();
+       
     }
 }
