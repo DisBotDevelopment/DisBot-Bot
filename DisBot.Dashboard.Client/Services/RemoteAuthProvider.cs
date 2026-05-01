5a1ac78390930a722968c32c92dc27a665a6af05
jesperrichert <jesper.l.richert@gmail.com>
2026-05-01 13:33:58 +0200

    updated some structures for ui.

--- DisBot.Dashboard.Client/Services/RemoteAuthProvider.cs
+++ DisBot.Dashboard.Client/Services/RemoteAuthProvider.cs
@@ -4,7 +4,6 @@ using System.Security.Claims;
 using DisBot.Shared.Http.Responses.Auth;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.JSInterop;
 using SerializationContext = DisBot.Shared.SerializationContext;
 
 namespace DisBot.Dashboard.Client.Services;
